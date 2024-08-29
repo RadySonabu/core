62ba5d835ed4c456037cea6396d8580b04a28261
RadySonabu <ardynubanos@yahoo.com>
2024-08-30 04:12:20 +0800

    Added django-cors-headers

--- core/settings.py
+++ core/settings.py
@@ -48,17 +48,21 @@ INSTALLED_APPS = [
     "apps.branches",
     "bootstrap5",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "core.urls"
 
